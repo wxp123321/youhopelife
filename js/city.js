760ebeee00746287e9f5c2d85c5422c45359d0f1
1648557071@qq.com <1648557071@qq.com>
2018-05-30 13:46:06 +0800

    切换城市

--- js/city.js
+++ js/city.js
@@ -1,4 +1,7 @@
 $(function () {
+    var city = getUrlParam('city');
+    $('.y-city').html(city);
+
     $('.city-ul li').on('mouseover',function () {
         $('.city-ul li').css('background','#fff');
         $('.first-span').css('color','#666');
@@ -7,4 +10,18 @@ $(function () {
         $($(this).children()[0]).css('color','#fff');
         $($(this).children()[0]).css('background','rgb(0,140,225)');
     });
+
+    $('.city-ul span').on('click',function () {
+       window.location = './index.html?city='+$(this).html();
+    });
+
+    $('.hot-city a').on('click',function () {
+        window.location = './index.html?city='+$(this).html();
+    });
 })
+
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return decodeURI(r[2]); return null; //返回参数值
+}
--- js/index.js
+++ js/index.js
@@ -198,4 +198,19 @@ $(function () {
     $('#sy').on('click',function () {
         window.location = './index.html';
     });
+
+    $('.my-city').on('click',function () {
+       window.location = 'city.html?city='+$('.my-city').html();
+    });
+
+
+
+    var city = getUrlParam('city');
+    $('.my-city').html(city);
 })
+
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return decodeURI(r[2]); return null; //返回参数值
+}
